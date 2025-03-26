d236c5eebbf0f8e4406404f82c423e06810fc2b7
ferym26 <ferym26@gmail.com>
2025-03-26 23:14:04 +0400

    feat: elemet-plus installed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Element Plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import ru from 'element-plus/dist/locale/ru.mjs'
+
 import './assets/styles/index.scss'
 
 const app = createApp(App)
+
 app.use(router)
+app.use(ElementPlus, {
+	locale: ru,
+})
+
 app.mount('#app')
