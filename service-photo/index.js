55a3cb62f0a3fd96c9cecdc46430f4e7e9ffb780
ferym26 <ferym26@gmail.com>
2025-09-15 21:28:35 +0400

    out service

--- service-photo/index.js
+++ service-photo/index.js
@@ -41,7 +41,6 @@ app.get('/api/avatar/:userId', async (req, res) => {
 	}
 })
 
-const PORT = 8585
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+	console.log(`Server running on port ${process.env.PORT}`)
 })
